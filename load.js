0a239d6c54c503994adb4e11c92c431d4733eacd
Matej398 <matej.resman@gmail.com>
2025-02-26 22:14:22 +0100

    Pushing to git

--- load.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const fadeElements = document.querySelectorAll('.fade-in');
-    fadeElements.forEach((element, index) => {
-        element.style.animationDelay = `${index * 0.2}s`; // 0.2s delay between each
-    });
-});
